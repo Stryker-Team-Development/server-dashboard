6d02362b0d832cdb30e87c4e8af3b7685b2e15aa
Jose Alvarez <joseluh_1@hotmail.com>
2021-02-22 09:37:11 -0500

    Fixing error message condition, removing unused files and refactoring css

--- src/App.js
+++ src/App.js
@@ -13,15 +13,23 @@ function App() {
     state: 'Please click Refresh button'
   });
 
-  const SERVER_STATUS_URL = 'https://ljf7a447tk.execute-api.us-east-1.amazonaws.com/prod/server'
+  const SERVER_STATUS_URL = process.env.REACT_APP_SERVER_STATUS_URL;
 
   function toggleServerState() {
     const valueToSet = isServerStopped() ? 'on' : 'off';
     axios.post(SERVER_STATUS_URL + '/state', {
       state: valueToSet
     }).then((response) => {
-      alert((response.data.success && valueToSet === 'on') ? 'Prendiendo esta monda' : 'Algo se fue a la verga, culpa de Alejo Ochoa');
-    })
+      if (response.data.success) {
+        if (valueToSet === 'on') {
+          alert('Prendiendo esta monda');
+        } else if (valueToSet === 'off') {
+          alert('Apagando la monda');
+        }
+      } else {
+        alert('Algo se fue a la verga, culpa de Alejo Ochoa')
+      }
+    });
   }
 
   function getServerStatus() {
@@ -43,23 +51,23 @@ function App() {
 
   return (
     <div className='App'>
-      <Card style={{ padding: '70px', width: '18rem', margin: '0 auto', whiteSpace: 'pre-line', display: 'flex', backgroundColor: (isServerPendingOrStopping() || isServerStopped()) === true ? "red" : "green" }}>
-      {serverStatus.state.toUpperCase()}
+      <Card className='server-status' style={{ backgroundColor: (isServerPendingOrStopping() || isServerStopped()) === true ? "red" : "green" }}>
+        {serverStatus.state.toUpperCase()}
         <br />
         {serverStatus.public_ip}
       </Card>
-      <Button style={{ margin: '50px' }}
+      <Button className='button'
         variant='primary'
         onClick={() => toggleServerState()}
         disabled={isServerPendingOrStopping() === true ? true : false}>
         {isServerStopped() ? 'Turn On' : 'Turn Off'}
       </Button>{' '}
-      <Button style={{ margin: '50px' }}
+      <Button className='button'
         variant='success'
         onClick={() => getServerStatus()}>
         Refresh
       </Button>{' '}
-      <Card style={{ marginTop: '50px', padding: '100px', width: '50rem', margin: '0 auto' }}>
+      <Card className='mono-kakos-paragraph'>
         Kakos es gay y el mono es un caca LETSS GOOOOOOOOOOOOOOOOOOOOO
       </Card>
     </div>
