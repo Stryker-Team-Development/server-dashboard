824cf058c387d3356cd01fd2610ead91886ec410
Jose Luis Alvarez Herrera <joseluh_1@hotmail.com>
2021-02-24 16:17:45 -0500

    Adding Discord iframe

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import Card from '@material-ui/core/Card';
 import './App.css';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import ServerStatus from './components/ServerStatus';
 import YoutubeCard from './components/YoutubeCard';
-import classNames from 'classnames';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
   mainCard: {
     margin: '0 auto',
     whiteSpace: 'pre-line',
     display: 'flex',
-  },
-  card: {
-    margin: 'auto',
-    maxWidth: 350,
-    height: 20,
-    zIndex: 1,
-    marginTop: 25,
-    border: '1px solid #d4d4d4',
-    borderRadius: 0,
-    webkitBoxShadow: '0 2px 4px 1px rgba(0,0,0,0.15);',
-    mozBoxShadow: '0 2px 4px 1px rgba(0,0,0,0.15);',
-    boxShadow: '0 2px 4px 1px rgba(0,0,0,0.15);',
-    marginBottom: '50px',
-    padding: '80px'
   }
 }));
 
@@ -40,20 +20,27 @@ function App() {
 
   return (
     <div className='App'>
-      <Container maxWidth="lg" className={classes.container}>
-        <Grid container spacing={3} justify="center" alignItems="center">
-          <Grid item xs={6}>
+      <Container maxWidth="lg" >
+      <Grid container spacing={3} justify="center" alignItems="center">
+          <Grid item xs={5}>
             <ServerStatus className={classes.mainCard}/>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={7}>
             <YoutubeCard className={classes.mainCard}/>
           </Grid>
         </Grid>
       </Container>
-      <Container maxWidth="false" className={classes.container}>
-        <Card className={classNames(classes.card)}>
-          Kakos es gay y el mono es un caca LETSS GOOOOOOOOOOOOOOOOOOOOO
-        </Card>
+      <Container maxWidth="false" >
+        <Grid container spacing={1} justify="center" alignItems="center">
+          <Grid item xs={5}>
+          <iframe src="https://discord.com/widget?id=254270297899925514&theme=dark"
+                  width="250"
+                  height="390"
+                  allowtransparency="true"
+                  frameborder="0"
+                  sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
